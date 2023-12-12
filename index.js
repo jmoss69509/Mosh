cc9ee63916c75a21928430586c0207f78a54716e
jmoss69509 <jmoss69509@aol.com>
2023-12-12 16:27:11 +0000

    Added index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// A comment !
+// document.write('Hello World2');
+console.log('This should be in Console Window');
+let firstName = ('John');
+console.log(firstName);
+const interestRate = 0.3;
+interestRate = 1;
+console.log (interestRate);
