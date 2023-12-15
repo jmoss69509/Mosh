37ebfe3d9bcefa2f85954a2064bf5b119dc73dfd
jmoss69509 <jmoss69509@aol.com>
2023-12-15 18:38:04 +0000

    231215 Small function messabout

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ console.log('This should be in Console Window');
 let firstName = ('John');
 console.log(firstName);
 const interestRate = 0.3;
-interestRate = 1;
+//interestRate = 1;
 console.log (interestRate);
+function Hello(){
+    console.log ('Hello World!');
+    console.log (Date.length());
+}
+
+Hello();
+console.log ('The End');
