b069683aa26dabd600b7402d3b5fe3020ea4f81e
jmoss69509 <jmoss69509@aol.com>
2024-02-04 20:57:19 +0000

    240204 FreeCodeCamp

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// A comment !
-// document.write('Hello World2');
-console.log('This should be in Console Window');
-let firstName = ('John');
-console.log(firstName);
-const interestRate = 0.3;
-//interestRate = 1;
-console.log (interestRate);
-function Hello(){
-    console.log ('Hello World!');
-    console.log (Date.length());
+function wordBlanks (myNoun, myAdjective, myVerb, myAdverb){
+    myNoun = 
+    document.write (`The ${myAdjective.toUpperCase()} ${myNoun} ${myVerb} to Sainsburys ${myAdverb}.<br>`);
 }
-
-Hello();
-console.log ('The End');
+wordBlanks ("dog","big","ran","quickly");
+wordBlanks   ("monster","small","limped","slowly");
+wordBlanks ("baby", "fat", "crawled", "like a snail");
+let firstName = "Frederico";
+let lastName = "Iglasias";
+document.write (`My name is ${firstName} ${lastName}.`);
