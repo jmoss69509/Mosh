aca7c22160d707b6343909b10c61d8ab6bf55c58
jmoss69509 <jmoss69509@aol.com>
2024-02-05 21:52:22 +0000

    240205 Dont forget your backticks !

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ function wordBlanks (myNoun, myAdjective, myVerb, myAdverb){
 wordBlanks ("dog","big","ran","quickly");
 wordBlanks   ("monster","small","limped","slowly");
 wordBlanks ("baby", "fat", "crawled", "like a snail");
-let firstName = "Frederico";
-let lastName = "Iglasias";
-document.write (`My name is ${firstName} ${lastName}.`);
+var firstName = "Frederico";
+var lastName = "Iglasias";
+document.write (`My name is ${firstName} ${lastName}.<br>`);
+// Arrays
+
+var myArray = [["This is my array", 23], [["Nested!"],["Something Else", "Second Sentence", "Third"]]];
+//document.write (`${myArray[0]}, and I am ${myArray[0,1]} years old. ${firstName} ${lastName}<br>`);
+//myArray[1] = ("Another string");
+document.write (`${myArray[0][0]}, and I am ${myArray[1]} years old. ${firstName} ${lastName}<br>`);
+document.write (`${myArray [1][1][0]}<br>`);
+var myNewPushArray = ["This ","is a ", "test"];
+myNewPushArray.push ("to ", "prove ","it ","works.");
+document.write(`${myNewPushArray} <br>`);
+
+function arrayPrinter(arrayInput){
+    for (count = 0; count <=arrayInput.length-1; ++count){
+        document.write (`${arrayInput[count]} ${count}<br>`);
+    }
+}
+arrayPrinter(myNewPushArray);
